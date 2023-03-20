d1d0addb19946c9325cf8f6f5ba599848d198313
udo <udo@openfu.com>
2023-03-20 02:57:18 +0100

    SDL example

--- tests/sdl2.c
+++ tests/sdl2.c
@@ -50,11 +50,7 @@
 
 int main(int argc, char* argv[])
 {
-    // Unused argc, argv
-    (void) argc;
-    (void) argv;
 
-    // Initialize SDL
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("SDL could not be initialized!\n"
@@ -62,16 +58,9 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-#if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
-    // Disable compositor bypass
     if(!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0"))
-    {
-        printf("SDL can not disable compositor bypass!\n");
-        return 0;
-    }
-#endif
+        printf("Warning: SDL can not disable compositor bypass\n");
 
-    // Create window
     SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
     SDL_Window *window = SDL_CreateWindow("Basic C SDL project",
                                           SDL_WINDOWPOS_UNDEFINED,
@@ -82,73 +71,51 @@ int main(int argc, char* argv[])
     {
         printf("Window could not be created!\n"
                "SDL_Error: %s\n", SDL_GetError());
-    }
-    else
-    {
-        // Create renderer
-        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if(!renderer)
-        {
-            printf("Renderer could not be created!\n"
-                   "SDL_Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            // Declare rect of square
-            SDL_Rect squareRect;
-
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-            // Square position: In the middle of the screen
-            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
-
-            // Event loop exit flag
-            bool quit = false;
-
-            // Event loop
-            f32 tick = 0;
-            while(!quit)
-            {
-				tick += 1.0/60.0;
-                SDL_Event e;
-
-                while (SDL_PollEvent(&e))
-				{
-					if(e.type == SDL_QUIT)
-						quit = true;
-                }
-
-                // Initialize renderer color white for the background
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-                // Clear screen
-                SDL_RenderClear(renderer);
-
-                // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
-                // Draw filled square
-                squareRect.w = (int)(200.0 + 100.0*sin(6.28*tick));
-                squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-                SDL_RenderFillRect(renderer, &squareRect);
-
-                // Update screen
-                SDL_RenderPresent(renderer);
-            }
-
-            // Destroy renderer
-            SDL_DestroyRenderer(renderer);
-        }
-
-        // Destroy window
-        SDL_DestroyWindow(window);
+        return 0;
     }
 
-    // Quit SDL
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if(!renderer)
+	{
+		printf("Renderer could not be created!\n"
+			   "SDL_Error: %s\n", SDL_GetError());
+        return 0;
+	}
+
+	SDL_Rect squareRect;
+
+	squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+	squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+	squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
+	squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
+
+	bool quit = false;
+	f32 tick = 0;
+
+	while(!quit)
+	{
+		tick += 1.0/60.0;
+		SDL_Event e;
+
+		while (SDL_PollEvent(&e))
+		{
+			if(e.type == SDL_QUIT)
+				quit = true;
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+
+		squareRect.w = (int)(200.0 + 100.0*sin(6.28*tick));
+		squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
+		SDL_RenderFillRect(renderer, &squareRect);
+
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
